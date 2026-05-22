85561aefe453497d0ef78a84b3bc55aa9f54c2db
JAGT1806 <jagt1806@gmail.com>
2026-05-21 23:53:08 -0500

    feat: update data type value field mongo db

--- src/main/java/com/greenhouse/smart_backend/modules/iot/dto/request/VariableDTO.java
+++ src/main/java/com/greenhouse/smart_backend/modules/iot/dto/request/VariableDTO.java
@@ -2,12 +2,10 @@ package com.greenhouse.smart_backend.modules.iot.dto.request;
 
 import lombok.Data;
 
-import java.math.BigDecimal;
-
 @Data
 public class VariableDTO {
     private String name;
     private String unit;
-    private BigDecimal value;
+    private String value;
 
 }
